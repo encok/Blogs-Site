1baaf624d640ae62b931c17efd786909d76592cd
enock <enckkipronoh@gmail.com>
2021-09-29 16:20:57 +0300

    creating login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { TechblogsComponent } from './techblogs/techblogs.component';
 import { TopheadingComponent } from './topheading/topheading.component';
 import { AboutComponent } from './about/about.component';
 import { BlogsComponent } from './blogs/blogs.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
- {path:'', component:TopheadingComponent}, //home
- {path:'tech', component:TechblogsComponent}, //tech
- {path:'blogs', component:BlogsComponent}, //about
+ {path:'', redirectTo:'login', pathMatch:'full'},
+ {path:'login', component:LoginComponent},//login
+ {path:'signup', component:SignupComponent}, //signup 
+ {path:'home', component:TopheadingComponent}, //home
+//  {path:'tech', component:TechblogsComponent}, //tech
+ {path:'blogs', component:BlogsComponent}, //blogs
  {path:'about', component:AboutComponent}, //about
  
 
