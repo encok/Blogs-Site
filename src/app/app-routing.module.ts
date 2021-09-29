0e2452114825b32e66dd983787c69ee1e7258ff2
enock <enckkipronoh@gmail.com>
2021-09-29 17:38:35 +0300

    creating navabar and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,9 +8,9 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
- {path:'', redirectTo:'login', pathMatch:'full'},
- {path:'login', component:LoginComponent},//login
- {path:'signup', component:SignupComponent}, //signup 
+//  {path:'', redirectTo:'login', pathMatch:'full'},
+//  {path:'login', component:LoginComponent},//login
+//  {path:'signup', component:SignupComponent}, //signup 
  {path:'home', component:TopheadingComponent}, //home
 //  {path:'tech', component:TechblogsComponent}, //tech
  {path:'blogs', component:BlogsComponent}, //blogs
